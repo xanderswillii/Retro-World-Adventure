70dfb694c530fb5d9f2323322ab0c507409d4b8e
xanderswillii <chedjouwilliam465@gmail.com>
2026-05-09 16:21:38 +0100

    adding keyboard inputs for movement and fixing player movement bug

--- player/player.gd
+++ player/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const GRAVITY = 1000
 
@@ -7,14 +8,17 @@ enum State { Idle, Run}
 var current_state = State.Idle
 
 func _ready():
-	pass
+	current_state = State.Idle
 	
 
 func _physics_process(delta):
 	player_falling(delta)
 	player_idle(delta)
+	player_run(delta)
 	
 	move_and_slide()
+	
+	player_animation()
 	
 	
 func player_falling(delta):
@@ -25,3 +29,22 @@ func player_falling(delta):
 func player_idle(delta):
 	if is_on_floor():
 		current_state = State.Idle
+
+
+func player_run(delta):
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	
+	if direction:
+		velocity.x = direction * 300
+	else:
+		velocity.x = move_toward(velocity.x, 0, 300)
+
+	if direction !=0:
+		current_state = State.Run
+
+func player_animation():
+	if current_state == State.Idle:
+		animated_sprite_2d.play("idle")
+	elif current_state == State.Run:
+		animated_sprite_2d.play("run")
