a64f6961e00925aa756b4151f27be0a888af5060
xanderswillii <chedjouwilliam465@gmail.com>
2026-05-10 12:18:55 +0100

    added player jump state and jump animation and fixed bug on player jump
    the fixed bug on player jump is because when I initially implemented it it was not gibing or the player will jump so far that we can't even see him again

--- player/player.gd
+++ player/player.gd
@@ -2,8 +2,10 @@ extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const GRAVITY = 1000
-
-enum State { Idle, Run}
+const SPEED = 300
+const JUMP = -300
+const JUMP_HORIZONTAL = 100
+enum State { Idle, Run, Jump}
 
 var current_state = State.Idle
 
@@ -15,10 +17,13 @@ func _physics_process(delta):
 	player_falling(delta)
 	player_idle(delta)
 	player_run(delta)
+	player_jump(delta)
 	
 	move_and_slide()
 	
 	player_animation()
+	
+	print("State: ", State.keys()[current_state])
 	
 	
 func player_falling(delta):
@@ -36,15 +41,28 @@ func player_run(delta):
 	
 	
 	if direction:
-		velocity.x = direction * 300
+		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, 300)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	if direction !=0:
 		current_state = State.Run
+		animated_sprite_2d.flip_h = false if direction > 0 else true
 
-func player_animation():
+
+func player_jump(delta):
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP
+		current_state = State.Jump
+
+	if !is_on_floor() and current_state == State.Jump:
+		var direction = Input.get_axis("move_left", "move_right")
+		velocity.x += direction * JUMP_HORIZONTAL * delta
+
+func player_animation(): 
 	if current_state == State.Idle:
 		animated_sprite_2d.play("idle")
 	elif current_state == State.Run:
 		animated_sprite_2d.play("run")
+	elif current_state == State.Jump:
+		animated_sprite_2d.play("jump")
