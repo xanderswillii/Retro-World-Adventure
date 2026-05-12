a37dc57db1ed6942139e345bbf1dec6fcc74908a
xanderswillii <chedjouwilliam465@gmail.com>
2026-05-12 10:49:18 +0100

    fix parallax background error and addded delta time on player's script for different motions

--- player/player.gd
+++ player/player.gd
@@ -2,10 +2,13 @@ extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const GRAVITY = 1000
-@export var speed : int  = 300
-@export var jump : int  = -300
-@export var jump_horizontal : int  = 100
+@export var speed : int  = 1000
+@export var max_horizontal_speed: int = 300
+@export var slow_down_speed: int = 1700
 
+@export var jump : int  = -300
+@export var jump_horizontal_speed : int  = 1000
+@export var max_jump_horizontal_speed: int = 300
 
 enum State { Idle, Run, Jump}
 
@@ -47,9 +50,10 @@ func player_run(delta : float):
 	
 	
 	if direction:
-		velocity.x = direction * speed
+		velocity.x += direction * speed * delta
+		velocity.x = clamp(velocity.x, -max_horizontal_speed, max_horizontal_speed)
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, slow_down_speed * delta)
 
 	if direction !=0 and is_on_floor() :
 		current_state = State.Run
@@ -63,7 +67,8 @@ func player_jump(delta : float):
 
 	if !is_on_floor() and current_state == State.Jump:
 		var direction = input_movement()
-		velocity.x += direction * jump_horizontal * delta
+		velocity.x += direction * jump_horizontal_speed * delta
+		velocity.x = clamp(velocity.x,  -max_jump_horizontal_speed,  max_jump_horizontal_speed)
 		velocity.x = clamp(velocity.x, -speed, speed)
 		if direction !=0:
 			animated_sprite_2d.flip_h = direction < 0
