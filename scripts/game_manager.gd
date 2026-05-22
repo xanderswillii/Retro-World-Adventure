27caf7cb11330e8118c608633fc33edfc08bdf6f
xanderswillii <chedjouwilliam465@gmail.com>
2026-05-22 15:26:53 +0100

    created pause menu scrfeen and methods in the game manager script

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,4 +1,23 @@
 extends Node
+var level_1 = preload("res://levels/level_1.tscn")
+
+
+
+
 
 func _ready():
 	RenderingServer.set_default_clear_color(Color(0.44,0.12,0.50,0.99))
+
+
+func start_game():
+	transition_to_scene(level_1.resource_path)
+	
+func exit_game():
+		get_tree().quit()
+	
+	
+func transition_to_scene(scene_path):
+	await get_tree().create_timer(0.1).timeout
+	get_tree().change_scene_to_file(scene_path)
+	
+	
