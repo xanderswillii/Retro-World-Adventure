37f0bdece52753ccc22e6a207e9e4a29f5d26f17
xanderswillii <chedjouwilliam465@gmail.com>
2026-05-24 00:38:19 +0100

    created the UI setting andstyled the option button also fixing issues with the pause button

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,8 +1,9 @@
 extends Node
+
+var main_menu_screen = preload("res://ui/main_menu_screen.tscn")
+
+var pause_menu_screen = preload("res://ui/pause_menu_screen.tscn")
 var level_1 = preload("res://levels/level_1.tscn")
-
-
-
 
 
 func _ready():
@@ -10,11 +11,29 @@ func _ready():
 
 
 func start_game():
+	if get_tree().paused:
+		continue_game()
+		return
+	
 	transition_to_scene(level_1.resource_path)
 	
 func exit_game():
-		get_tree().quit()
+	get_tree().quit()
 	
+	
+func pause_game():
+	get_tree().paused = true
+	
+	var pause_menu_screen_instance = pause_menu_screen.instantiate()
+	get_tree().get_root().add_child(pause_menu_screen_instance)
+	
+func continue_game():
+	get_tree().paused = false
+	
+	
+func main_menu():
+	var main_menu_screen_instance = main_menu_screen.instantiate()
+	get_tree().get_root().add_child(main_menu_screen_instance)
 	
 func transition_to_scene(scene_path):
 	await get_tree().create_timer(0.1).timeout
---
+++ ui/settings_menu_screen.gd
@@ -0,0 +1,33 @@
+extends CanvasLayer
+
+@onready var window_mode_option_button: OptionButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/WindowModeOptionButton
+@onready var resolution_option_button: OptionButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/ResolutionOptionButton
+
+var window_modes : Dictionary = {"Fullscreen" : DisplayServer.WINDOW_MODE_FULLSCREEN,
+									"Window" : DisplayServer.WINDOW_MODE_WINDOWED, 
+									 "Window Maximized" : DisplayServer.WINDOW_MODE_MAXIMIZED }
+
+var resolutions : Dictionary ={"320x180" : Vector2i(230, 180),
+							  "480x270" : Vector2i(480, 270),
+							  "640x360" : Vector2i(640, 360),
+							  "854x480" : Vector2i(640, 360),
+							  "1280x720"  : Vector2i(1280, 720)}
+
+func _ready():
+	for window_mode in window_modes:
+		window_mode_option_button.add_item(window_mode)
+		
+	for resolution in resolutions:
+		resolution_option_button.add_item(resolution)
+
+
+func _on_window_mode_option_button_item_selected(index: int) -> void:
+	var window_mode = window_modes.get(window_mode_option_button.get_item_text(index))
+	print("window mode", window_mode)
+
+func _on_resolution_option_button_item_selected(index: int) -> void:
+	var resolution = resolution.get(resolution_option_button.get_item_text(index)) as Vector2i
+	print("resolution", resolution)
+
+func _on_main_menu_button_pressed() -> void:
+	queue_free()
