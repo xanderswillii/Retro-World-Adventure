8b29c433cbcdad58e6a455f8cb957cb76440000c
xanderswillii <chedjouwilliam465@gmail.com>
2026-06-01 00:14:18 +0100

    created a scene transition screen
    it displays when you begin the game and when you switch levels.

--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var scene_transition_screen = preload("res://ui/screen_transition/scene_transition_screen.tscn")
+
 var scenes : Dictionary = { "level1": "res://levels/level_1.tscn",
 							"level2": "res://levels/level_2.tscn"}
 
@@ -8,5 +10,8 @@ func _transition_to_scene(level : String):
 	var scene_path : String = scenes.get(level)
 	
 	if scene_path != null:
-		await get_tree().create_timer(1.0).timeout
+		var scene_transition_screen_instance = scene_transition_screen.instantiate()
+		get_tree().get_root().add_child(scene_transition_screen_instance)
+		await get_tree().create_timer(4.0).timeout
 		get_tree().change_scene_to_file(scene_path)
+		scene_transition_screen_instance.queue_free()
