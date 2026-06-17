9ccbfef8c8d4a986407f783d63b998129f57c1ab
xanderswillii <chedjouwilliam465@gmail.com>
2026-06-17 02:12:30 +0100

    continued level 5 design and created a bat with different states
    To control the states, I am using a state machine to enable the player to be able to transition from one state to another.

---
+++ enemies/bats/bats.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+var enemy_death_effect = preload("res://enemies/enemy_death_effect.tscn")
+
+@onready var animated_sprite_2d : AnimatedSprite2D = $AnimatedSprite2D
+@onready var state_machine : NodeFiniteStateMachine = $StateMachine
+
+@onready var damage_amount : int = 1
+@onready var health_amount : int = 3
+
+
+
+func _on_hurt_box_area_entered(area: Area2D) -> void:
+	if area.get_parent().has_method("get_damage_amount"):
+		var node = area.get_parent() as Node
+		health_amount -= node.damage_amount
+		print("Bath health:", health_amount)
+		
+	if health_amount <= 0:
+		state_machine.transition_to("death")
+	else:
+		state_machine.transition_tp("hurt")
